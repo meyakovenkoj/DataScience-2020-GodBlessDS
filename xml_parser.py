0e8347851a0c9688c7a7bf4bc86abf66c7372514
Yurkov Kirill <vk1r1ll@yandex.ru>
2020-10-17 22:38:04 +0300

    xml and yaml CAPS for Airports, deleted status for flight

--- xml_parser.py
+++ xml_parser.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import csv
 from lxml import etree as et
-result_file = open('PointzAggregator-AirlinesData2.csv', 'w', newline='')
+result_file = open('PointzAggregator-AirlinesData.csv', 'w', newline='')
 writer = csv.writer(result_file, delimiter=",")
 first_part = []
 second_part = []
 third_part = []
 row = ['userid','firstname','lastname','cardstype','cardnumber','bonusprogramm',
-'activitiesType','actType','actCode','actDate','actDeparture','actArrival','actFare']
+'activitiesType','actType','actPlane','actDate','actDeparture','actArrival','actFare','Class']
 writer.writerow(row)
 
 with open('PointzAggregator-AirlinesData.xml') as xml_data:
@@ -35,7 +35,7 @@ with open('PointzAggregator-AirlinesData.xml') as xml_data:
                 second_part.append(card_root.xpath("./activities/@type")[0])
                 if len(list(card_root.iter('activity'))) == 0:
                     third_part.clear()
-                    third_part = ['', '', '', '', '', '']
+                    third_part = ['', '', '', '', '', '','']
                     row = first_part + second_part + third_part
                     #print(row)
                     writer.writerow(row)
@@ -46,9 +46,10 @@ with open('PointzAggregator-AirlinesData.xml') as xml_data:
                         third_part.append(activity_root.xpath("./@type")[0])
                         third_part.append(activity_root.xpath("./Code/text()")[0])
                         third_part.append(activity_root.xpath("./Date/text()")[0])
-                        third_part.append(activity_root.xpath("./Departure/text()")[0])
-                        third_part.append(activity_root.xpath("./Arrival/text()")[0])
+                        third_part.append((activity_root.xpath("./Departure/text()")[0]).upper())
+                        third_part.append((activity_root.xpath("./Arrival/text()")[0]).upper())
                         third_part.append(activity_root.xpath("./Fare/text()")[0])
+                        third_part.append('')#for Class
                         row = first_part + second_part + third_part
                         #print(row)
                         writer.writerow(row)
--- yaml_parser.py
+++ yaml_parser.py
@@ -9,8 +9,8 @@ fr = ''
 status = ''
 to = ''
 first_line = 1
-result_file = open('SkyTeam-Exchange2.csv', 'w')
-result_file.write('Date,Plane,cardnumber,Class,actFare,actDeparture,Status,actArrival\n')
+result_file = open('SkyTeam-Exchange.csv', 'w')
+result_file.write('actDate,actPlane,cardnumber,Class,actFare,actDeparture,actArrival\n')
 with open('SkyTeam-Exchange.yaml') as fileobject:
     for line in fileobject:
         yaml_line = yaml.safe_load(line)
@@ -24,13 +24,11 @@ with open('SkyTeam-Exchange.yaml') as fileobject:
             s = key+','+value['CLASS']+','+value['FARE']
             flights.append(s)
         elif key == 'FROM':
-            fr = value
-        elif key == 'STATUS':
-            status = value
+            fr = value.upper()
         elif key == 'TO':
-            to = value
+            to = value.upper()
             for i in range(len(flights)):
-                row = date+','+plane+','+flights[i]+','+fr+','+status+','+to+'\n'
+                row = date+','+plane+','+flights[i]+','+fr+','+to+'\n'
                 result_file.write(row)
                 #print(row)
             flights = []
