9faa598f5c316581685ee692772e8d76f0008da1
Rodion Sharafiev <catinquest@mail.ru>
2020-10-17 14:20:57 +0300

    fixed for excluding not interesting columns and add encoding='UTF8'

--- tab_parser/tab_parser.py
+++ tab_parser/tab_parser.py
@@ -2,31 +2,36 @@
 # so if you want to recognize them you have to set column sizes depend on what you see in different srtings of .tab file
 COL_SIZES = [60, 12, 12, 12, 12, 12, 6, 6, 6, 6, 6, 18, 12, 6, 6, 6, 6, 12, 24, 36, 60] 
 
+EXCLUDE_COL_NUM = [13, ]
+
 if __name__ == "__main__":
 
-        file_desc = open('orig_data/Sirena-export-fixed.tab', 'r')
+        file_desc = open('orig_data/Sirena-export-fixed.tab', 'r', encoding='UTF8')
 
-        new_file_desc = open('orig_data/out/Sirena-export-fixed.csv','w')
+        new_file_desc = open('orig_data/out/Sirena-export-fixed.csv','w', encoding='UTF8')
 
         unnamed_columns = 0
         i = 0
 
         for line in file_desc:
                 new_line = ''
+                j = 0
                 for lenth in COL_SIZES:
                         col_content = line[:lenth]
                         line = line[lenth:]
-                        col_content = col_content.strip()
-                        if not col_content:
-                                if not i:
-                                        unnamed_columns += 1
-                                        col_content = 'UnnamedColumn' + str(unnamed_columns)
+                        if j not in EXCLUDE_COL_NUM:
+                                col_content = col_content.strip()
+                                if not col_content:
+                                        if not i:
+                                                unnamed_columns += 1
+                                                col_content = 'UnnamedColumn' + str(unnamed_columns)
+                                        else:
+                                                col_content = 'Null'
+                                if not new_line:
+                                        new_line = col_content
                                 else:
-                                        col_content = 'Null'
-                        if not new_line:
-                                new_line = col_content
-                        else:
-                                new_line = new_line + ';' + col_content
+                                        new_line = new_line + ';' + col_content
+                        j += 1
                 if not i:
                         new_file_desc.write(new_line + '\n\n')
                         i = 1
