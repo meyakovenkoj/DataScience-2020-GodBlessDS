acc52ab978f096d83d5aea1cf4c9d54c5605a46a
Rodion Sharafiev <rodion12344321@mail.ru>
2020-10-18 23:38:12 +0300

    fixed csv b day filler (was problem with the same persons)

--- csv_b_day_filler.py
+++ csv_b_day_filler.py
@@ -15,10 +15,17 @@ with open(sys.argv[1], 'r', encoding='UTF8') as in_file, open(sys.argv[1][:-3] +
                 for i in lst:
                     line = line + ';' + i
                 seen.add(line)
-            elif line not in seen:
+            else:
                 bad.add(line)
         else:
             out_file.write(line)
     for line in bad:
-        if line not in seen: # if data not inseen but in bad - there was no data with b-day for such person
+        lst = line.split()
+        line = lst[0]
+        lst = lst[1:3] + ['',] + lst[4:] # data without b-day
+        for i in lst:
+            line = line + ';' + i
+        if line not in seen: # if data not in seen but in bad - there was no data with b-day for such person
             out_file.write(line)
+
+out_file.close()
