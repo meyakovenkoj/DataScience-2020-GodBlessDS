d7840f0b4070496421c1173c45c8bbbcf7e0ce24
Ivan Yakovenko <meyakovenkoj@yandex.ru>
2020-10-20 01:33:58 +0300

    fix cleaner

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -4,6 +4,7 @@ import sys
 with open(sys.argv[1], 'r', encoding='UTF8') as in_file, open(sys.argv[1][:-3] + 'clean.csv', 'w', encoding='UTF8') as out_file:
     seen = set()
     for line in in_file:
+        line = line.strip()
         if line not in seen:
             seen.add(line)
-            out_file.write(line)
+            print(line, file=out_file)
