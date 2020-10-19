6888ee573a80fd56e8910736e96f6475d32bcb67
Rodion Sharafiev <rodion12344321@mail.ru>
2020-10-19 21:57:10 +0300

    fixed loosing \n

--- csv_b_day_filler.py
+++ csv_b_day_filler.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import copy
 
 with open(sys.argv[1], 'r', encoding='UTF8') as in_file, open(sys.argv[1][:-3] + 'clean.csv', 'w', encoding='UTF8') as out_file:
     seen = set()
@@ -20,12 +21,13 @@ with open(sys.argv[1], 'r', encoding='UTF8') as in_file, open(sys.argv[1][:-3] +
         else:
             out_file.write(line)
     for line in bad:
+        line2 = copy.copy(line)
         lst = line.split()
         line = lst[0]
         lst = lst[1:3] + ['',] + lst[4:] # data without b-day
         for i in lst:
             line = line + ';' + i
         if line not in seen: # if data not in seen but in bad - there was no data with b-day for such person
-            out_file.write(line)
+            out_file.write(line2)
 
 out_file.close()
