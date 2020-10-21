c931aa0a81b45cc8ce93fb082d11eb255e5e5f56
Yakovenko Ivan <32681800+meyakovenkoj@users.noreply.github.com>
2020-10-21 13:07:58 +0300

    add updates

--- db_scripts/insert_in_merged_flights_Razm.sql
+++ db_scripts/insert_in_merged_flights_Razm.sql
@@ -21,6 +21,42 @@ FROM xlsx_flight
 WHERE xlsx_flight.id not in (SELECT xlsx_f_id FROM flight_merged)
 
 
+UPDATE flight_merged
+SET json_f_id = xlf.id, from_abr = xlf.departure, dest_abr = xlf.arrival
+FROM flight_merged fm JOIN json_flight xlf
+	ON xlf.flight_code=fm.flight_code
+	AND xlf.fl_date=fm.depart_date;
+
+
+INSERT INTO flight_merged(xlsx_f_id,flight_code,depart_date,dest_abr,from_abr)
+SELECT DISTINCT id,flight_code,fl_date,arrival,departure
+FROM json_flight
+WHERE json_flight.id not in (SELECT json_f_id FROM flight_merged);
+
+
+UPDATE flight_merged
+SET xml_f_id = xlf.flight_id, from_abr = xlf.Departure, dest_abr = xlf.Arrival
+FROM flight_merged fm JOIN xml_flight xlf
+	ON xlf.flightCode=fm.flight_code
+	AND xlf.Date=fm.depart_date;
+
+INSERT INTO flight_merged(xml_f_id,flight_code,depart_date,dest_abr,from_abr)
+SELECT DISTINCT flight_id,flightCode,Date,Arrival,Departure
+FROM xml_flight
+WHERE xml_flight.flight_id not in (SELECT xml_f_id FROM flight_merged);
+
+UPDATE flight_merged
+SET tab_f_id = xlf.flight_id, from_abr = xlf.[From], dest_abr = xlf.Dest, arrival_date=xlf.ArrivalDate, depart_time=xlf.DepartTime, arrival_time=xlf.ArrivalTime
+FROM flight_merged fm JOIN tab_flight xlf
+	ON xlf.Flight=fm.flight_code
+	AND xlf.DepartDate=fm.depart_date;
+
+INSERT INTO flight_merged(xml_f_id,flight_code,depart_date,arrival_date,dest_abr,from_abr, arrival_time, depart_time)
+SELECT DISTINCT flight_id,Flight,DepartDate,ArrivalDate, Dest, [From], ArrivalTime, DepartTime
+FROM tab_flight
+WHERE tab_flight.flight_id not in (SELECT tab_f_id FROM flight_merged);
+
+
 
 
 
